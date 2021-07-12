95020719275d7615cbdfe6cf508cd58b810bd3e6
wildmaples <maple.ong@shopify.com>
2021-07-11 20:30:53 -0400

    Add solution for looping triangle

---
+++ chapter-2/LoopingTriangle.js
@@ -0,0 +1,12 @@
+// Write a loop that makes seven calls to console.log to output the following triangle:
+// #
+// ##
+// ###
+// ####
+// #####
+// ######
+// #######
+
+for (let index = 1; index < 8; index++) {
+  console.log("#".repeat(index));
+}
